2b6810f706c719d478a6a759f11f3c876f82b17f
Geonerd <geonerding@gmail.com>
2017-03-29 09:40:27 -0600

    footer fix

--- core.js
+++ core.js
@@ -18,10 +18,7 @@ require([
     var mapBaseLayer = new WebTileLayer({
         urlTemplate: "http://{subDomain}.tile.stamen.com/terrain/{level}/{col}/{row}.png",
         subDomains: ["a", "b", "c", "d"],
-        copyright: "Map tiles by <a href=\"http://stamen.com/\">Stamen Design</a>, " +
-            "under <a href=\"http://creativecommons.org/licenses/by/3.0\">CC BY 3.0</a>. " +
-            "Data by <a href=\"http://openstreetmap.org/\">OpenStreetMap</a>, " +
-            "under <a href=\"http://creativecommons.org/licenses/by-sa/3.0\">CC BY SA</a>."
+        copyright: "Map tiles by Stamen Design."
     });
 
     // Create a Basemap with the WebTileLayer. The thumbnailUrl will be used for
