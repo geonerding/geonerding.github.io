f84c83a15c5f3625fd5bced791e16e0ed75d5153
Geonerd <geonerding@gmail.com>
2017-08-24 13:30:28 -0600

    Printing URL data to div

--- pcireport.js
+++ pcireport.js
@@ -1,9 +1,10 @@
 // Get url as variable
 var webby = window.location.href;
 var name = "str";
+var obj = "obj";
 var length = "length";
 var width = "width";
-var stat = "status";
+var condition = "condition";
 
 function getVal( name, url ) {
     if (!url) url = location.href;
@@ -14,3 +15,16 @@ function getVal( name, url ) {
     return results == null ? null : results[1];
 }
 
+function getEm( name ) {
+    if (!webby) webby = location.href;
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( webby );
+    return results == null ? null : results[1];
+}
+
+function popHead() {
+	var streetName = getEm(name);
+	document.getElementById('head').innerHTML = streetName;
+}
